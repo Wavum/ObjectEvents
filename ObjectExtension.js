041ac9c3ae373cf69b608d2abbdb19a69d3f4336
Wavum <rl00@gmx.at>
2017-03-02 22:05:28 +0100

    Update ObjectExtension.js

--- ObjectExtension.js
+++ ObjectExtension.js
@@ -2,12 +2,13 @@ Object.prototype.CustomEventListeners = new Array();
 
 Object.prototype.listenToEvent = function listenToEvent(eventName, callback)
 {
-    this.CustomEventListeners.push([eventName, callback]);
+    this.CustomEventListeners.push([eventName, this, callback]);
 };
 
 Object.prototype.unlistenFromEvent = function unlistenFromEvent(eventName, callback)
 {
-    var index = this.CustomEventListeners.indexOf([eventName, callback]);
+    var index = this.CustomEventListeners.indexOf([eventName, this, callback]);
+
     this.CustomEventListeners.splice(index, 1);
 };
 
@@ -15,9 +16,10 @@ Object.prototype.fireEvent = function fireEvent(eventName)
 {
     for (var i = 0; i < this.CustomEventListeners.length; i++)
     {
-        if (this.CustomEventListeners[i][0] === eventName)
+        if (this.CustomEventListeners[i][0] === eventName &&
+            this.CustomEventListeners[i][1] == this)
         {
-            this.CustomEventListeners[i][1](this);
+            this.CustomEventListeners[i][2](this);
         }
     }
 };
